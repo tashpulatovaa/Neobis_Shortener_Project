c8fc105a9806a0b937fb3ab3f41d7cf0c7633e20
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2023-08-13 20:20:33 +0600

    create short_urls model and home func in view

--- url_app/models.py
+++ url_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class short_urls(models.Model):
+    short_url = models.CharField(max_length=20)
+    long_url = models.URLField("URL")
--- url_app/views.py
+++ url_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import short_urls
 
 # Create your views here.
+
+def Home(request, token):
+    urls = short_urls.objects.all()
+    print(urls.values())
+    for i in urls:
+        print(i)
+    long_url = short_urls.objects.filter(short_url=token)[0]
+    return redirect(long_url.long_url)
